3cc89b87df5b663da964835e65524ce769f68d58
Andrew Koidan <deathangel908@gmail.com>
2019-03-29 16:46:40 +0200

    fixed

--- src/types/vue-property.d.ts
+++ src/types/vue-property.d.ts
@@ -1,9 +1,16 @@
 import {Logger} from 'lines-logger';
+import Vue from 'vue/types/vue';
 
 
 declare module 'vue/types/vue' {
 
     interface Vue {
         logger: Logger;
+    }
+}
+
+declare module 'vue/types/options' {
+    interface ComponentOptions<V extends Vue> {
+        _componentTag?: string;
     }
 }
